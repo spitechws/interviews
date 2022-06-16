2917fb04b56c9f3f2861cc375e840e455c06a439
SPSONI <sheetala.prasadsoni@pressganey.com>
2022-06-16 22:11:36 +0530

    commit

--- reference-globe/partials/header-app.php
+++ reference-globe/partials/header-app.php
@@ -13,15 +13,8 @@ if (empty($_SESSION['user']->user_id)) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="<?PHP echo BASE_URL ?>assets/css/style.css" rel="stylesheet">
+  <link href="<?php echo BASE_URL ?>assets/css/style.css" rel="stylesheet">
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <script type="text/javascript">
-    var BASE_URL = '<?php echo BASE_URL; ?>';
-    var UPLOAD_PATH = '<?php echo UPLOAD_PATH; ?>';
-    var UPLOAD_URL = '<?php echo UPLOAD_URL; ?>';
-    var API_BASE_URL = '<?php echo API_BASE_URL; ?>';
-  </script>
-  <script src="<?PHP echo BASE_URL ?>assets/js/app_functions.js"></script>
 </head>
 
 <body>

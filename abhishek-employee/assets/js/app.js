0865c0df26ca190166525648483ee7d03b3c6b90
SPSONI <sheetala.prasadsoni@pressganey.com>
2022-07-12 12:49:26 +0530

    commit

--- abhishek-employee/assets/js/app.js
+++ abhishek-employee/assets/js/app.js
@@ -3,9 +3,7 @@ function load_employee(search_key, response_container_id) {
         url: BASE_URL + 'php/live_search.php',
         method: "POST",
         dateType: 'json',
-        data: {
-            data: { search_key: search_key }
-        },
+        data: { search_key: search_key },
         success: function (data) {
             $("#" + response_container_id).html(data);
         },
--- abhishek-employee/php/live_search.php
+++ abhishek-employee/php/live_search.php
@@ -1,49 +1,47 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/config.php');
 
-if (isset($_POST['data'])) {
-    $data = $_POST['data'];
-    $query = "SELECT * FROM employee detailsi WHERE employee_name LIKE '{$data}%'";
-    $result = mysqli_query($conn, $query);
-    if (mysqli_num_rows($result) > 0) { ?>
-        <table class="table tabel-bordered table-striped mt-4">
-            <thead>
-                <tr class="customers">
-                    <th>ID</th>
-                    <th>EmployeName</th>
-                    <th>Designation</th>
-                    <th>Grade</th>
-                    <th>Permission</th>
+$data = $_POST['search_key'];
+$query = "SELECT * FROM employee  WHERE employee_name LIKE '{$data}%'";
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) { ?>
+    <table class="table tabel-bordered">
+        <thead>
+            <tr class="customers">
+                <th>#</th>
+                <th>Select</th>
+                <th>Employ eName</th>
+                <th>Designation</th>
+                <th>Grade</th>
+                <th>Permission</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $i = 1;
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row['id'];
+                $employee_name = $row['employee_name'];
+                $designation = $row['designation'];
+                $grade     = $row['grade'];
+                $permission = $row['permission'];
+            ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><input type="checkbox" name="select[]" id="emp<?php echo $row['id'] ?>" value="<?php echo $row['id'] ?>"></td>
+                    <td><?php echo $employee_name; ?></td>
+                    <td><?php echo $designation; ?></td>
+                    <td><?php echo $grade; ?></td>
+                    <td><?php echo $permission; ?></td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $id = $row['id'];
-                    $employee_name = $row['employee_name'];
-                    $designation = $row['designation'];
-                    $grade     = $row['grade'];
-                    $permission = $row['permission'];
-                ?>
-                    <tr>
 
-                        <td><?php echo $id; ?></td>
-                        <td><?php echo $employee_name; ?></td>
-                        <td><?php echo $designation; ?></td>
-                        <td><?php echo $grade; ?></td>
-                        <td><?php echo $permission; ?></td>
-                    </tr>
-
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
+            <?php
+                $i++;
+            }
+            ?>
+        </tbody>
+    </table>
 <?php
-    } else {
-        echo "No Data Found";
-    }
 } else {
-    echo "hello";
+    echo "No Data Found";
 }
-?>

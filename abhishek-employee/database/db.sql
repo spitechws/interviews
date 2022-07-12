b028632934cdf81bb70d6237ccedeed9de8a3684
SPSONI <sheetala.prasadsoni@pressganey.com>
2022-07-12 13:25:46 +0530

    Update db.sql

--- abhishek-employee/database/db.sql
+++ abhishek-employee/database/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 11, 2022 at 07:42 AM
+-- Generation Time: Jul 12, 2022 at 07:55 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.4.9
 
@@ -30,20 +30,20 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE IF NOT EXISTS `employee` (
   `id` int NOT NULL,
-  `employee_name` varchar(255) NOT NULL,
-  `designation` varchar(225) NOT NULL,
-  `grade` varchar(10) NOT NULL,
-  `permission` varchar(225) NOT NULL,
+  `employee_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `designation` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
+  `grade` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `permission` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employee`
 --
 
 INSERT INTO `employee` (`id`, `employee_name`, `designation`, `grade`, `permission`) VALUES
-(0, 'EMP.1', 'Software Engineer', 'L1', ''),
-(1, 'EMP.2', 'Quality Engineer', 'L1', '');
+(1, 'EMP.2', 'Quality Engineer', 'L1', ''),
+(2, 'EMP.1', 'Software Engineer', 'L1', '');
 
 -- --------------------------------------------------------
 
@@ -54,9 +54,9 @@ INSERT INTO `employee` (`id`, `employee_name`, `designation`, `grade`, `permissi
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(255) NOT NULL,
+  `project_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `project`
@@ -71,17 +71,45 @@ INSERT INTO `project` (`id`, `project_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+CREATE TABLE IF NOT EXISTS `team` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `project_id` int NOT NULL,
+  `emp_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`id`, `project_id`, `emp_id`) VALUES
+(1, 1, 0),
+(2, 1, 1),
+(3, 2, 1),
+(4, 2, 2),
+(5, 2, 1),
+(6, 2, 2),
+(7, 2, 1),
+(8, 2, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
